083afa4fadcb59f3f3f05c076f74cc61e89e7e39
Israel Lugo <hostelixisrael@gmail.com>
2018-01-08 23:31:53 -0400

    [MOD] add function is_command

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ import {
 } from './data';
 
 const get_data_localbitcoins = (url, pages) => {
-	
+
     const payload = {
         fields: 'public_view,min_amount,max_amount,temp_price,bank_name,currency,location_string'
     };
@@ -16,50 +16,55 @@ const get_data_localbitcoins = (url, pages) => {
         params: payload
     };
 
-    return new Promise(function(resolve, reject) {
-    
-        axios.get(url, options).then((response)=>{
+    return new Promise(function (resolve, reject) {
+
+        axios.get(url, options).then((response) => {
 
             let data = response.data;
 
             pages = pages.concat([data.data.ad_list]);
 
             let url_next_page = data.pagination.next;
-            
-            if(url_next_page){
+
+            if (url_next_page) {
                 resolve(get_data_localbitcoins(url_next_page, pages));
             }
             else {
                 resolve(pages);
             }
         })
-        .catch((err)=>{
-            reject(err);
-        });
+            .catch((err) => {
+                reject(err);
+            });
     });
-}
+};
 
 
 const is_name_bank = (mesg) => {
     return banks.map(name_bank => name_bank.toLowerCase()).indexOf(mesg) !== -1;
-}
+};
 
 const is_country_code = (mesg) => {
     return countries_code.map(c_country => c_country.toLowerCase()).indexOf(mesg) !== -1;
-}
+};
 
 const is_country = (mesg) => {
     return countries.map(country => country.toLowerCase()).indexOf(mesg) !== -1;
-}
+};
 
 const is_currency = (mesg) => {
     return currencies.map(currency => currency.toLowerCase()).indexOf(mesg) !== -1;
-}
+};
+
+const is_command = (mesg, commands) => {
+    return commands.map(command => command.toLowerCase()).indexOf(mesg) !== -1;
+};
 
 export {
     get_data_localbitcoins,
     is_name_bank,
     is_country_code,
     is_country,
-    is_currency
+    is_currency,
+    is_command
 }
