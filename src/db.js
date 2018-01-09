c05825d4a5af96eb8b1220a63c25185b594a6447
Israel Lugo <hostelixisrael@gmail.com>
2018-01-09 12:05:34 -0400

    [MOD] migrate lowdb => dbservice

--- src/db.js
+++ src/db.js
@@ -1,7 +1,20 @@
+import lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
 import config from '../config/config';
+
+const adapter = new FileSync(config.pathDB);
 
 class DBService {
     constructor() {
+        this.db = lowdb(adapter);
+
+        if (!this.db.has(config.collectionUsers).value()) {
+            this.db.set(config.collectionUsers, []).write();
+        }
+    }
+
+    getDB() {
+        return this.db;
     }
 }
 
